5f415b37ba94c397b74d59290f98d84697034b79
moljac <mcvjetko@holisticware.net>
2013-04-19 22:59:47 +0200

    TypeInfoAmbiguity bug Android and Windows samples added

---
+++ mono/Xamarin.Android.TypeInfoAmbiguity/TypeInfoAmbiguity.Android/TypeInfoConsumer.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+using SomeNamespace;
+using System.Reflection;
+
+namespace TypeInfoAmbiguity
+{
+	public class TypeInfoConsumer
+	{
+		public void f(TypeInfo ti)
+		{
+			//System.Reflection.Assembly asm = new System.Reflection.Assembly();
+		}
+	}
+}
